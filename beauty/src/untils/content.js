f496176b7edfe1e5d0932ad6fa4e73074b8660f6
tongyuehong1 <245503112@qq.com>
2017-10-28 20:39:31 +0800

    商城首页（wx: for content）

--- beauty/src/untils/content.js
+++ beauty/src/untils/content.js
@@ -1,6 +1,6 @@
 module.exports = [
   {
-    img: [
+    photo: [
       'http://img1.imgtn.bdimg.com/it/u=850119793,4249750756&fm=27&gp=0.jpg',
       'http://img5.imgtn.bdimg.com/it/u=4120270162,516095231&fm=27&gp=0.jpg',
       'http://img1.imgtn.bdimg.com/it/u=1207912580,634887275&fm=27&gp=0.jpg',
@@ -9,12 +9,12 @@ module.exports = [
     title: '水果'
   },
   {
-    img: [
+    photo: [
       'http://img1.imgtn.bdimg.com/it/u=850119793,4249750756&fm=27&gp=0.jpg',
       'http://img5.imgtn.bdimg.com/it/u=4120270162,516095231&fm=27&gp=0.jpg',
       'http://img1.imgtn.bdimg.com/it/u=1207912580,634887275&fm=27&gp=0.jpg',
       'http://img5.imgtn.bdimg.com/it/u=2229099749,611550402&fm=27&gp=0.jpg'
     ],
-    title: '水果'
+    title: '坚果'
   }
 ]
